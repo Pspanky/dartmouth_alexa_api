9983561240ac515b8425148fa818206ab764225c
Pspanky <paul.e.spangfort.18@dartmouth.edu>
2017-05-31 07:09:02 -0400

    fixing html email

--- app/controllers/email_controller.js
+++ app/controllers/email_controller.js
@@ -20,7 +20,7 @@ export const sendEmail = (req, res) => {
     from: 'halendrtesting@gmail.com',
     to: req.body.email,
     subject: 'Welcome to Halendr!',
-    html: '<div style="color: blue"> Hey </div>',
+    html: '<div style="width:700px; background-color:white; font-family: sans-serif"><div style="text-align:center"><b>Confirm your registration</b></div><br><div style="text-align:center">Thanks for signing up. We look forward to connecting you to the things around you.</div><br><br><div></div><div><div><b>Didn’t sign up? </b></div>If you didn’t sign up for an account on Halendr, please let us know.<div></div></div><br><br><div style="text-align:center">We hope you enjoy your experience on Halendr. If you need help using Halendr, please visit our Help Centre. You can also contact us or report a problem, here.</div></div>',
   });
 
   const newUser = new User();
