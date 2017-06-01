87b233c631c4c483708e15fdf38e39e907da4733
Pspanky <paul.e.spangfort.18@dartmouth.edu>
2017-06-01 05:28:02 -0400

    send copy  of registered users to own email

--- app/controllers/email_controller.js
+++ app/controllers/email_controller.js
@@ -19,8 +19,15 @@ export const sendEmail = (req, res, history) => {
   transporter.sendMail({
     from: 'no-reply@halendr.com',
     to: req.body.email,
-    subject: 'Halendr Registration Confirmed!',
-    html: '<div style="width:700px; background-color:white; font-family: sans-serif"><img src="https://s11.postimg.org/bw1ik7qpf/Halendr_Logo_1.png" style="width: 260px; display: block; margin: auto" /><br/><br/><br/><div style="text-align:center"><b>Registration Confirmed!</b></div><br/><div style="text-align:center">Thank you for signing up. We look forward to connecting you to the things you love.</div><br/><br/><div></div><div style="position: relative; left: 20px;"><div ><b>Didn’t sign up? </b></div>If you didn’t sign up for an account on Halendr, please let us know.<div></div></div><br/><br/><div style="text-align:center">We hope you enjoy your experience on Halendr. If you need help using Halendr, please visit our Help Centre.</div><br/><div style="width: 80%; height: 1px; background-color: rgba(255,65,43, 0.4); margin: auto;"></div></div>',
+    subject: 'Hello from Halendr!',
+    html: '<div style="width:700px; background-color:white; font-family: sans-serif"><img src="https://s11.postimg.org/bw1ik7qpf/Halendr_Logo_1.png" style="width: 260px; display: block; margin: auto" /><br/><br/><br/><div style="text-align:center"><b>Registration Confirmed!</b></div><br/><div style="text-align:center">Thanks for signing up. We\'re excited that you\'re interested in discovering everything happening around you and we can\'t wait to see you on 01 July 2017. </div><br/><br/><div></div><div style="position: relative; left: 20px;"><div></div></div><br/><br/><div style="text-align:center">We\'ll be in touch with exciting updates closer to our launch.</div><br/><div style="width: 80%; height: 1px; background-color: rgba(255,65,43, 0.4); margin: auto;"></div></div>',
+  });
+
+  transporter.sendMail({
+    from: 'no-reply@halendr.com',
+    to: 'no-reply@halendr.com',
+    subject: 'New User Registered',
+    text: req.body.email,
   });
 
   const newUser = new User();
