5657c3c784787df59257d5fc400ab99075ad2339
Pspanky <paul.e.spangfort.18@dartmouth.edu>
2017-06-11 13:24:45 +0800

    updated launch date

--- app/controllers/email_controller.js
+++ app/controllers/email_controller.js
@@ -39,7 +39,7 @@ export const sendEmail = (req, res, history) => {
         from: 'no-reply@halendr.com',
         to: req.body.email,
         subject: 'Hello from Halendr!',
-        html: '<div style="width:700px; background-color:white; font-family: sans-serif"><img src="https://s11.postimg.org/bw1ik7qpf/Halendr_Logo_1.png" style="width: 260px; display: block; margin: auto" /><br/><br/><br/><div style="text-align:center"></div><br/><div style="text-align:center">Thank you for signing up! We\'re excited that you\'re interested in discovering what\'s happening around you and we can\'t wait to see you on 01 July 2017. </div><br/><br/><div></div><div style="position: relative; left: 20px;"><div></div></div><br/><br/><div style="text-align:center">We\'ll be in touch with exciting updates closer to our launch.</div><br/><div style="width: 80%; height: 1px; background-color: rgba(255,65,43, 0.4); margin: auto;"></div></div>',
+        html: '<div style="width:550px; background-color:white; font-family: sans-serif"><img src="https://s11.postimg.org/bw1ik7qpf/Halendr_Logo_1.png" style="width: 260px; display: block; margin: auto" /><br/><br/><div style="text-align:center"></div><br/><div style="text-align:center">Thank you for signing up! We\'re excited that you\'re interested in discovering what\'s happening around you and we can\'t wait to see you on 01 September 2017. </div><br/><div></div><div style="position: relative; left: 20px;"><div></div></div><br/><br/><div style="text-align:center">We\'ll be in touch with exciting updates closer to our launch.</div><br/><div style="width: 80%; height: 1px; background-color: rgba(255,65,43, 0.4); margin: auto;"></div></div>',
       });
 
       newUser.save().then((addedUser) => {
